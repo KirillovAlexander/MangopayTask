cdb7e41f54b47053cd3cfa1ea703bbea74f331e4
Kirillov Alexander <kirillovbrest@gmail.com>
2023-08-07 22:26:09 +0200

    Add README.md and some basic swagger description

---
+++ README.md
@@ -0,0 +1,28 @@
+# Idea Backend Service
+
+Welcome to the Idea backend service!
+This Java Spring Boot application allows you to create and view your ideas.
+
+## Features
+
+- Create and view your ideas through the provided REST API.
+- Secure access to your ideas using Google OAuth2 authentication.
+- Easy-to-use Swagger documentation for the API: [http://localhost:8080/swagger-ui/index.html](http://localhost:8080/swagger-ui/index.html).
+
+## Getting Started
+
+Follow these steps to get started with managing your ideas:
+
+1. Clone this repository to your local machine.
+
+2. Ensure you have Docker installed.
+
+3. Run MongoDB using Docker Compose:
+   ```sh
+   docker-compose up -d
+
+4. Build and run the Spring Boot application.
+
+5. Access the Swagger documentation to explore the API endpoints and their functionality.
+
+6. Use Google OAuth2 authentication to secure your ideas and data.
--- src/main/java/com/verycoolapp/idea/rest/IdeaResource.java
+++ src/main/java/com/verycoolapp/idea/rest/IdeaResource.java
@@ -2,6 +2,7 @@ package com.verycoolapp.idea.rest;
 
 import com.verycoolapp.idea.dto.IdeaDTO;
 import com.verycoolapp.idea.service.IdeaService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 import java.security.Principal;
 import java.util.List;
 
-@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/ideas")
@@ -18,11 +18,13 @@ public class IdeaResource {
 
     private final IdeaService ideaService;
 
+    @Operation(summary = "Get all ideas for the current user")
     @GetMapping
     public List<IdeaDTO> getAllIdeas(final Principal principal) {
         return ideaService.getAllIdeas(principal);
     }
 
+    @Operation(summary = "Create a new idea for the current user")
     @PostMapping
     public IdeaDTO createIdea(@Valid @RequestBody final IdeaDTO ideaDTO, final Principal principal) {
         return ideaService.createIdea(ideaDTO, principal);
